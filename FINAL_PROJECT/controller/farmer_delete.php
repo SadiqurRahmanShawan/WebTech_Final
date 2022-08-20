b3d60d079cc3af9571477c09ee7b475528d888f7
Sadiq <sadikshawan@gmail.com>
2022-08-20 14:45:01 +0600

    deleted project from lab folder

--- FINAL_PROJECT/controller/farmer_delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    include '../model/db_connection.php';
-
-    $sql = "DELETE FROM `users` WHERE `ID` =".$_GET["user_id"];
-    $row = mysqli_query($con,$sql);
-
-    header("Location:../view/profile.php");
-?>
